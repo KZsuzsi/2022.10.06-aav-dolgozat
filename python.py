88b92192a7e44bdb6076b152348f349e191ac6e7
KZsuzsi <doctorzsu@gmail.com>
2022-10-06 15:54:55 +0200

    first commit

---
+++ python.py
@@ -0,0 +1,4 @@
+#________________________A háromszög területe______________
+a=int(input("Kérem a háromszög oldalának a hosszát: "))
+m=int(input("Kérem a háromszög magasságát: "))
+print("A háromszög területe: ", (m*a)/2)
